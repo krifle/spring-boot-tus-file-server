03ae5eab60b9da322fe8b4958636d03e4cd391f2
JHKim <JHKim>
2017-04-15 15:06:06 +0900

    first commit

---
+++ src/main/java/com/krifle/SpringBootTusFileServerApplication.java
@@ -0,0 +1,12 @@
+package com.krifle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTusFileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTusFileServerApplication.class, args);
+	}
+}
